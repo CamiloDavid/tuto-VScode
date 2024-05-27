cd7b4ff8cc14f7484b1bdaf90af3ba05fc3d6bef
SaintAlejo <camilodavid9910@gmail.com>
2024-05-27 11:20:42 -0500

    commit inicial

---
+++ Ejercicio1/calcular_edad_segun_fecha_nacimiento.py
@@ -0,0 +1,18 @@
+print('hola mundo')
+
+my_number = '1'
+my_string = 'Hola'
+
+my_variable = my_number + my_string
+print(my_variable)
+
+print(f'Mi variable es: {my_variable}')
+
+print(f'hola mundo: {my_variable}')
+
+
+
+
+
+
+
